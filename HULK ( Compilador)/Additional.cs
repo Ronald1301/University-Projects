609bc461b5195ffb45d9464851f2a8f4eae8a7bb
Ronald1301 <provanceronald@gmail.com>
2023-12-15 22:14:24 -0500

    listo

--- HULK ( Compilador)/Additional.cs
+++ HULK ( Compilador)/Additional.cs
@@ -6,7 +6,7 @@ namespace Hulk
          // Diccionario donde asocio algunos tipos de token con las expresiones que conforman
         public static Dictionary<Token, Expressions> Global_variables = new Dictionary<Token, Expressions>();
         public static List<FunctionDeclarations> functionDeclarations = new List<FunctionDeclarations>();
-        public static Dictionary<string, FunctionDeclarations> Functions_global = new Dictionary<string, List<FunctionDeclarations>();
+        public static Dictionary<string, FunctionDeclarations> Functions_global = new Dictionary<string, FunctionDeclarations>();
         public static Dictionary<string, List<Token>> FuncDeclaredParams = new Dictionary<string, List<Token>>();
         public static Dictionary<string, Expressions> FuncDeclaredBody = new Dictionary<string, Expressions>();
         public static string error = "";
